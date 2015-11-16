f565f0cc9485ca1d7763399975fb448b6fe7b428
katzenpapst <praecipitator@gmx.net>
2015-11-16 20:31:16 +0100

    included ctmlib

--- src/main/java/de/katzenpapst/amunra/client/renderer/RendererMultiOre.java
+++ src/main/java/de/katzenpapst/amunra/client/renderer/RendererMultiOre.java
@@ -2,10 +2,9 @@ package de.katzenpapst.amunra.client.renderer;
 
 import org.lwjgl.opengl.GL11;
 
+import team.chisel.ctmlib.Drawing;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import cpw.mods.fml.client.registry.RenderingRegistry;
@@ -26,13 +25,13 @@ public class RendererMultiOre implements ISimpleBlockRenderingHandler {
 		GL11.glDisable(GL11.GL_LIGHTING);
 
 		// and then the overlay
-				drawBlock(block, ((BlockOreVariable)block).getActualBlockIcon(), renderer);
+		Drawing.drawBlock(block, ((BlockOreVariable)block).getActualBlockIcon(), renderer);
 
 
 		//drawBlock(block, ((BlockCarvableLayered)block).getBaseTex(), renderer);
 		GL11.glEnable(GL11.GL_LIGHTING);
 		// draw the subblock first
-				drawBlock(block, metadata, renderer);
+		Drawing.drawBlock(block, metadata, renderer);
 
 		GL11.glTranslatef(0.5F, 0.5F, 0.5F);
 
@@ -56,66 +55,6 @@ public class RendererMultiOre implements ISimpleBlockRenderingHandler {
 	@Override
 	public int getRenderId() {
 		return AmunRa.multiOreRendererId;
-	}
-
-	// TODO include the ctmlib instead: http://www.curse.com/mc-mods/minecraft/232154-ctmlib
-	// https://github.com/TheCricket/Chisel/blob/1.7/dev/build.gradle like this?
-	public static void drawBlock(Block block, IIcon icon, RenderBlocks renderer) {
-		Tessellator tessellator = Tessellator.instance;
-
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(0.0F, -1.0F, 0.0F);
-		renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, icon);
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(0.0F, 1.0F, 0.0F);
-		renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, icon);
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(0.0F, 0.0F, -1.0F);
-		renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, icon);
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(0.0F, 0.0F, 1.0F);
-		renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, icon);
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(-1.0F, 0.0F, 0.0F);
-		renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, icon);
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(1.0F, 0.0F, 0.0F);
-		renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, icon);
-		tessellator.draw();
-	}
-
-	public static void drawBlock(Block block, int meta, RenderBlocks renderer) {
-		Tessellator tessellator = Tessellator.instance;
-
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(0.0F, -1.0F, 0.0F);
-		renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, meta));
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(0.0F, 1.0F, 0.0F);
-		renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(1, meta));
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(0.0F, 0.0F, -1.0F);
-		renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(2, meta));
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(0.0F, 0.0F, 1.0F);
-		renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(3, meta));
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(-1.0F, 0.0F, 0.0F);
-		renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(4, meta));
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(1.0F, 0.0F, 0.0F);
-		renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(5, meta));
-		tessellator.draw();
 	}
 
 }
