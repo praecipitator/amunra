bd40749e9fa562ce465f3c37aa8b8a1df337cf3c
katzenpapst <praecipitator@gmx.net>
2015-12-25 17:05:26 +0100

    testing stuff

---
+++ src/main/java/de/katzenpapst/amunra/world/mapgen/volcano/Volcano.java
@@ -0,0 +1,15 @@
+package de.katzenpapst.amunra.world.mapgen.volcano;
+
+import java.util.Random;
+
+import de.katzenpapst.amunra.world.mapgen.BaseStructureStart;
+import net.minecraft.world.World;
+
+public class Volcano extends BaseStructureStart {
+
+	public Volcano(World world, int chunkX, int chunkZ, Random rand) {
+		super(world, chunkX, chunkZ, rand);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/de/katzenpapst/amunra/world/mapgen/volcano/VolcanoGenerator.java
@@ -0,0 +1,47 @@
+package de.katzenpapst.amunra.world.mapgen.volcano;
+
+import java.util.Random;
+
+import de.katzenpapst.amunra.world.mapgen.BaseStructureStart;
+import de.katzenpapst.amunra.world.mapgen.StructureGenerator;
+import net.minecraft.util.MathHelper;
+
+public class VolcanoGenerator extends StructureGenerator  {
+
+	@Override
+	protected long getSalt() {
+		return 84375932847598L;
+	}
+
+	@Override
+	protected boolean canGenerateHere(int chunkX, int chunkZ, Random rand) {
+		int rangeShift = 2;
+		int range = 1 << rangeShift;
+		int superchunkX = chunkX >> rangeShift;
+		int superchunkZ = chunkZ >> rangeShift;
+
+		int chunkStartX = superchunkX << rangeShift;
+		int chunkStartZ = superchunkZ << rangeShift;
+		int chunkEndX = chunkStartX+range-1;
+		int chunkEndZ = chunkStartZ+range-1;
+		// this square of chunk coords superchunkX,superchunkX+range-1 and superchunkZ,superchunkZ+range-1
+		// now could contain a village
+		this.rand.setSeed(this.worldObj.getSeed() ^ this.getSalt() ^ superchunkX ^ superchunkZ);
+
+		int actualVillageX = MathHelper.getRandomIntegerInRange(this.rand, chunkStartX, chunkEndX);
+		int actualVillageZ = MathHelper.getRandomIntegerInRange(this.rand, chunkStartZ, chunkEndZ);
+
+		return (chunkX == actualVillageX && chunkZ == actualVillageZ);
+	}
+
+	@Override
+	protected BaseStructureStart createNewStructure(int xChunkCoord, int zChunkCoord) {
+		return new Volcano(worldObj, xChunkCoord, zChunkCoord, rand);
+	}
+
+	@Override
+	public String getName() {
+		return "Volcano";
+	}
+
+}
