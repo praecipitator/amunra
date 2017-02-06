0200c0ad66b4c0a3901c63a3fc8d17512dbf1c5d
katzenpapst <praecipitator@gmx.net>
2017-02-06 19:43:54 +0100

    version stuf

--- src/main/java/de/katzenpapst/amunra/AmunRa.java
+++ src/main/java/de/katzenpapst/amunra/AmunRa.java
@@ -89,7 +89,7 @@ public class AmunRa
 {
     public static final String MODID = "GalacticraftAmunRa";
     public static final String MODNAME = "Amun-Ra";
-    public static final String VERSION = "0.3.5";
+    public static final String VERSION = "0.3.6";
 
     public static ARChannelHandler packetPipeline;
 
