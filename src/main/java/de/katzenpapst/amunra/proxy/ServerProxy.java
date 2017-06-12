cede0b476f28ff5fceb7b7871fa3303c9f46d636
katzenpapst <praecipitator@gmx.net>
2017-06-12 23:07:51 +0200

    potentially fixed #43, made the mehen belt brighter

--- src/main/java/de/katzenpapst/amunra/proxy/ServerProxy.java
+++ src/main/java/de/katzenpapst/amunra/proxy/ServerProxy.java
@@ -10,10 +10,14 @@ public class ServerProxy extends ARSidedProxy {
     @Override
     public void preInit(FMLPreInitializationEvent event)
     {
-        MinecraftServer s = MinecraftServer.getServer();
-        if(s.isDedicatedServer() && !s.isServerInOnlineMode() && AmunRa.config.mothershipUserMatchUUID) {
-            GCLog.info("Server running in offline mode. Setting \"matchUsersByUUID\" to false");
-            AmunRa.config.mothershipUserMatchUUID = false;
+        try {
+            MinecraftServer s = MinecraftServer.getServer();
+            if(s.isDedicatedServer() && !s.isServerInOnlineMode() && AmunRa.config.mothershipUserMatchUUID) {
+                GCLog.info("Server running in offline mode. Setting \"matchUsersByUUID\" to false");
+                AmunRa.config.mothershipUserMatchUUID = false;
+            }
+        } catch (NullPointerException e) {
+            GCLog.info("Could not detect whenever server is in online mode. If it's not, please manually set \"matchUsersByUUID\" to false");
         }
     }
 
